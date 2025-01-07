ff5a22890ffa34b39ecc11c617689e5f57f8989b
Satyam Jha <satyam@trackier.com>
2025-01-07 10:47:33 +0530

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Ionic_Simulator',
+  webDir: 'www'
+};
+
+export default config;
